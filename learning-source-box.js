aa4475b822306850ff76ec9ee1e0d697ffba0c5c
brainboosthub <mintana9990@gmail.com>
2026-07-20 14:09:56 +0700

    Update learning-source-box.js

--- learning-source-box.js
+++ learning-source-box.js
@@ -3,7 +3,7 @@
 
   /* เปลี่ยน URL นี้เป็น URL /exec ของโปรเจกต์ฐานแหล่งเรียนรู้ */
   const LEARNING_SOURCE_WEB_APP_URL =
-    'https://script.google.com/macros/s/AKfycbxrisIGnewRP8cqrj_-Imv3KtkrXFu-cdUPHN2bqoYVpyJZaV1_K3U6d9o15uc6NBcJlw/exec';
+    'PASTE_LEARNING_SOURCE_WEB_APP_EXEC_URL_HERE';
   const API_URL = LEARNING_SOURCE_WEB_APP_URL + '?mode=learningBox';
 
   let sources = [];
